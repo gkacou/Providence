0ef3c6545e316b9ce4e9e7d6a68ed113c5bad5fa
Georges KACOU <georges.kacou@genesis.ci>
2019-07-30 00:50:12 +0000

    Test 1

--- providence_project/settings.py
+++ providence_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '4@f_b+a$&hxefe6irnl@&&*ud(+-v8*23#fw)ws7(c7=!0o#w#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
