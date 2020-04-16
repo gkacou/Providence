a6181de6061f6fcdcb5afbd4f81b37572082cc63
Georges KACOU <georges.kacou@genesis.ci>
2020-04-16 18:18:25 +0000

    Patch locale 2

--- blog/admin.py
+++ blog/admin.py
@@ -29,7 +29,7 @@ from .forms import CasCreationForm, CasChangeForm, CotisationChoiceField
 
 # Défintion du paramètre de localisation des affichages
 if platform == "linux" or platform == "linux2":
-    locale.setlocale(locale.LC_ALL, '')
+    locale.setlocale(locale.LC_ALL, 'fr_FR')
 elif platform == "darwin":
     locale.setlocale(locale.LC_ALL, 'fr_FR')
 elif platform == "win32":
