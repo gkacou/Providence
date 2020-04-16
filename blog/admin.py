6d617415eec28ca8de1e936dc365866093528222
Georges KACOU <georges.kacou@genesis.ci>
2020-04-16 17:28:57 +0000

    Locale

--- blog/admin.py
+++ blog/admin.py
@@ -28,7 +28,7 @@ from .forms import CasCreationForm, CasChangeForm, CotisationChoiceField
 
 
 if platform == "linux" or platform == "linux2":
-    locale.setlocale( locale.LC_ALL, 'fr_FR')
+    locale.setlocale( locale.LC_ALL, 'fr_FR.UTF-8')
 elif platform == "darwin":
     locale.setlocale( locale.LC_ALL, 'fr_FR')
 elif platform == "win32":
